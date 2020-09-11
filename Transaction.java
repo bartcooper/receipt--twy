94712c56db49baa83a16b44ba7973f139a28ea8d
bartcooper <60145975+bartcooper@users.noreply.github.com>
2020-09-11 09:34:15 -0400

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,1 +1,41 @@
+public class Transaction
+	Scanner scan = new Scanner(System.in);
+	public void greet(){
+		System.out.println("Welcome!");
+		System.out.println("Choose an item: ");
+	}
+	pubic void purchase(){
+		int total = 0;
+		boolean repeat = false
+		while (repeat == false) {
+			System.out.println("coffee(1), donut(2), or both(3)");
+			int coice = scan.nextInt();
+			if (choice==1)
+				System.out.println("Here is your coffee. That will be $4.")
+				total = total + 4;
+				System.out.println("Would you like anything else?");
+				String again = scan.nextString();
+				if (again == No || again == no \\ again == NO) {
+					repeat = false
+				}
+			}	
+			else if (choice==2) {
+				System.out.println("Here is your donut. That will be $3.")
+				total = total + 3;
+				System.out.println("Would you like anything else?");
+				String again = scan.nextString();
+				if (again == No || again == no \\ again == NO) {
+					repeat = false
+				}
+			}
+			else (choice==3) {
+				System.out.println("Here is your coffee and your donut. That will be $6.")
+				total = total + 4;
+				System.out.println("Would you like anything else?");
+				String again = scan.nextString();
+				if (again == No || again == no \\ again == NO) {
+					repeat = false
+				}
+			}
+		}
 
